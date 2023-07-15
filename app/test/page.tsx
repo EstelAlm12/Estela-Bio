f27b8e1d80c23276e0e62b904140281d2146cdfc
estela_almeida <estela_marialmeida@hotmail.com>
2023-07-15 13:40:35 -0700

    made pages tab

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function My_Test() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      this is a test
+    </main>
+  )
+}
